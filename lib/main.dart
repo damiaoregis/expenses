d94713119ad5064a7579578471187d5bba65be4a
damiao <damiao.v.serrana@gmail.com>
2021-09-26 23:53:42 -0300

    v2.1

--- lib/main.dart
+++ lib/main.dart
@@ -92,8 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    final appBar =  AppBar(
         title: Text('Despesas Pessoais'),
         centerTitle: true,
         actions: [
@@ -102,14 +101,26 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () =>_openTransactionFormModal(context),
           )
         ],
-      ),
+      );
+   
+    final availableHeight = MediaQuery.of(context).size.height - 
+    appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_transactions, _removeTrasaction),
+            Container(
+              height: availableHeight * 0.3,
+              child: Chart(_recentTransactions),
+              ),
+            Container(
+              height: availableHeight * 0.7 ,
+              child: TransactionList(_transactions, _removeTrasaction),
+              ),
           ],
         ),
       ),
